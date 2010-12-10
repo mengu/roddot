1b07ade27efc36330c5d84002e172e8ec33cb3f2
mengu <mengukagan@gmail.com>
2010-12-11 00:09:20 +0200

    created the qxroddot project.

---
+++ public/qxroddot/source/class/qxroddot/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxroddot.theme.Decoration",
+{
+  extend : qx.theme.modern.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ public/qxroddot/source/class/qxroddot/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxroddot.theme.Theme",
+{
+  meta :
+  {
+    color : qxroddot.theme.Color,
+    decoration : qxroddot.theme.Decoration,
+    font : qxroddot.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxroddot.theme.Appearance
+  }
+});
