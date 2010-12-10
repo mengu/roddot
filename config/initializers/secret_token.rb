9b0b6b3d7c25599c34a6e7e10525356c3dbea985
mengu <mengukagan@gmail.com>
2010-12-11 00:05:13 +0200

    initial commit for the roddot project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roddot::Application.config.secret_token = '6266324a723dcdc5204ea1358482ce33e47b4b952a8d4c510fd57ad0860b749334d7a0bd760083cd34b34ddfdb6ae48b324ee3b8bdd3d75dbb2f8578ca7f11d9'
