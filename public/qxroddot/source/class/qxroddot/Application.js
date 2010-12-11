edfee588e9fa382b773f94284049a914387fcd2f
mengu <mengukagan@gmail.com>
2010-12-11 03:48:31 +0200

    created the tree, added subreddits, and tons.

--- public/qxroddot/source/class/qxroddot/Application.js
+++ public/qxroddot/source/class/qxroddot/Application.js
@@ -1,10 +1,16 @@
 /* ************************************************************************
 
-   Copyright:
+    qxroddot - a simple reddit feed browser being built with qooxdoo and rails
 
-   License:
+    Copyright:
+        2010 - Muhammet S. AYDIN
+
+    License:
+        GPL: http://www.gnu.org/licenses/gpl.html
+        See the LICENSE file in the project's top-level directory for details.
 
    Authors:
+        Muhammet S. AYDIN (mengu)
 
 ************************************************************************ */
 
@@ -19,57 +25,117 @@
  */
 qx.Class.define("qxroddot.Application",
 {
-  extend : qx.application.Standalone,
+    extend : qx.application.Standalone,
 
 
 
-  /*
+    /*
   *****************************************************************************
      MEMBERS
   *****************************************************************************
   */
 
-  members :
-  {
-    /**
+    members :
+    {
+        /**
      * This method contains the initial application code and gets called 
      * during startup of the application
      * 
      * @lint ignoreDeprecated(alert)
      */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
+        main : function()
+        {
+            // Call super class
+            this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
+            // Enable logging in debug variant
+            if (qx.core.Variant.isSet("qx.debug", "on"))
+            {
+                // support native logging capabilities, e.g. Firebug for Firefox
+                qx.log.appender.Native;
+                // support additional cross-browser console. Press F7 to toggle visibility
+                qx.log.appender.Console;
+            }
 
-      /*
+            /*
       -------------------------------------------------------------------------
         Below is your actual application code...
       -------------------------------------------------------------------------
       */
+            // Document is the application root
+            var doc = this.getRoot();
+      
+            // container layout
+            var layout = new qx.ui.layout.VBox();
 
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "qxroddot/test.png");
+            // main container
+            var mainContainer = new qx.ui.container.Composite(layout);
+            doc.add(mainContainer, {
+                edge : 0
+            });
+      
+            mainContainer.add(new qxroddot.view.Header(), {
+                flex : 0
+            });
+      
+            var box = new qx.ui.layout.HBox();
+            var secondContainer = new qx.ui.container.Composite(box);
+            secondContainer.setMarginTop(20);
+            secondContainer.setMarginLeft(5);
+            mainContainer.add(secondContainer);
+      
+            redditTree = new qx.ui.tree.Tree().set({
+                width : 200,
+                height : 400
+            });
+            var reddits = new qx.ui.tree.TreeFolder("Reddits");
+            var programmingReddit = new qx.ui.tree.TreeFolder("Programming");
+            var pythonReddit = new qx.ui.tree.TreeFolder("Python");
+            var djangoReddit = new qx.ui.tree.TreeFolder("Django");
+            var rubyReddit = new qx.ui.tree.TreeFolder("Ruby");
+            var railsReddit = new qx.ui.tree.TreeFolder("Rails");
+            var scalaReddit = new qx.ui.tree.TreeFolder("Scala");
+            var clojureReddit = new qx.ui.tree.TreeFolder("Clojure");
+            var javaReddit = new qx.ui.tree.TreeFolder("Java");
+            var lispReddit = new qx.ui.tree.TreeFolder("Lisp");
+            reddits.add(programmingReddit, pythonReddit, djangoReddit, rubyReddit,
+                railsReddit, scalaReddit, clojureReddit, javaReddit, lispReddit);
+            reddits.setOpen(true);
+            redditTree.setRoot(reddits);
+            redditTree.setHideRoot(true);
+            secondContainer.add(redditTree);
+      
+            textLabel = new qx.ui.basic.Label().set({
+                rich : true,
+                width: 700
+            });
+            textLabel.setMarginLeft(10);
 
-      // Document is the application root
-      var doc = this.getRoot();
+            var scrollContainer = new qx.ui.container.Scroll();
+            scrollContainer.set({
+                width: 700,
+                height: 500,
+                marginLeft: 20
+            });
+            scrollContainer.add(textLabel);
+            secondContainer.add(scrollContainer);
 
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+            var treeItems = redditTree.getItems();
+            var treeItemCount = treeItems.length;
+            for (var i = 0; i < treeItemCount; ++i) {
+                treeItems[i].addListener("dblclick", function(e) {
+                    var selection = redditTree.getSelection();
+                    if (selection.length > 0) {
+                        var redditName = selection[0].getLabel().toLowerCase();
+                        var req = new qx.io.remote.Request("/feeds/"+redditName, "GET", "text/html");
+                        req.addListener("completed", function(evt) {
+                            textLabel.setValue(evt.getContent());
+                        })
+                        req.send();
+                    }
+                })
+            }
+      
+        }
     }
-  }
 });
