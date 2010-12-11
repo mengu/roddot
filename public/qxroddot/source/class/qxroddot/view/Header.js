6abfb7873c6a8b6c677ad6038b4af0960fa21828
mengu <mengukagan@gmail.com>
2010-12-11 03:46:00 +0200

    created the license.

--- public/qxroddot/source/class/qxroddot/view/Header.js
+++ public/qxroddot/source/class/qxroddot/view/Header.js
@@ -1,3 +1,18 @@
+/* ************************************************************************
+
+    qxroddot - a simple reddit feed browser being built with qooxdoo and rails
+
+    Copyright:
+        2010 - Muhammet S. AYDIN
+
+    License:
+        GPL: http://www.gnu.org/licenses/gpl.html
+        See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+        Muhammet S. AYDIN (mengu)
+
+************************************************************************ */
 /**
  * Application header widget.
  */
@@ -10,10 +25,10 @@ qx.Class.define("qxroddot.view.Header",
     this.base(arguments, new qx.ui.layout.HBox());
     this.setAppearance("app-header");
 
-    var versionTag = this.tr("A reddit app with Qooxdoo & Rails");
+    var appInfo = this.tr("A reddit app with Qooxdoo & Rails");
 
     this.add(new qx.ui.basic.Label(this.tr("Qx Roddot")));
     this.add(new qx.ui.core.Spacer, { flex : 1 });
-    this.add(new qx.ui.basic.Label(versionTag));
+    this.add(new qx.ui.basic.Label(appInfo));
   }
 });
