60e63de933aa19ea340c3d38e205b0f26f87acb2
mengu <mengukagan@gmail.com>
2011-08-15 04:07:48 +0300

    dunno what i was thinking when returning html within the controller.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -12,20 +12,9 @@ class FeedsController < ApplicationController
     @reddit_feed = ActiveSupport::JSON.decode(@feed)
     @titles = []
 
-    for @sub_feed in @reddit_feed["data"]["children"]
-      @title = '<div style="margin-bottom: 1px;"><a href="http://reddit.com'+@sub_feed["data"]["permalink"]+'" style="color: #000; text-decoration:none;" target="_blank">'+@sub_feed["data"]["title"]+'</a>'
-      @url = @sub_feed["data"]["url"]
-
-      if !@url.include?("r/"+params[:id])
-        @title << ' [<a href="'+@sub_feed["data"]["url"]+'" target="_blank">+</a>]'
-      end
-
-      @title << "</div>"
-      @titles << @title
-    end
-
+    
     respond_to do |format|
-      format.html { render :text => @titles.join("<br>")}
+      format.html
     end
 
   end
